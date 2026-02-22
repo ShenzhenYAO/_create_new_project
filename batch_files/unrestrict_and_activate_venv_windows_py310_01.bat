367d34c1a3bb6ef47a13977cd70d4640bda32601
shenzhenyao <junk.them@hotmail.com>
2026-02-21 16:39:43 -0800

    20260221c

--- batch_files/unrestrict_and_activate_venv_windows_py310_01.bat
+++ batch_files/unrestrict_and_activate_venv_windows_py310_01.bat
@@ -1,16 +1,16 @@
 @REM @echo off
-@REM powershell -NoExit -Command "Set-ExecutionPolicy -Scope Process -ExecutionPolicy Bypass -Force; & '.\venv_windows_py310_01\Scripts\Activate.ps1'"
-@REM echo PowerShell execution policy set to Bypass for the process and virtual environment activated.
+powershell -NoExit -Command "Set-ExecutionPolicy -Scope Process -ExecutionPolicy Bypass -Force; & '.\venv_windows_py310_01\Scripts\Activate.ps1'"
+echo PowerShell execution policy set to Bypass for the process and virtual environment activated.
+@REM @REM python -m pip install --upgrade pip
+@REM @REM pip install nbformat ipynbname
+@REM @REM pip install --upgrade ipywidgets jupyter
+
+
+@REM @echo off
+@REM REM Use 'call' to run the activation script and return to the batch flow
+@REM call ".\venv_windows_py310_01\Scripts\activate.bat"
+
+@REM echo Virtual environment activated.
 @REM python -m pip install --upgrade pip
 @REM pip install nbformat ipynbname
 @REM pip install --upgrade ipywidgets jupyter
-
-
-@echo off
-REM Use 'call' to run the activation script and return to the batch flow
-call ".\venv_windows_py310_01\Scripts\activate.bat"
-
-echo Virtual environment activated.
-python -m pip install --upgrade pip
-pip install nbformat ipynbname
-pip install --upgrade ipywidgets jupyter
